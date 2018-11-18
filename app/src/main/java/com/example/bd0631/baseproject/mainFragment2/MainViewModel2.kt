f9b081edc98db4dc9ffe96d61f92c983bf3d9244
Karolis <karolis.ziu@gmail.com>
2018-11-18 15:43:15 +0200

    add rxKotlin

--- app/src/main/java/com/example/bd0631/baseproject/mainFragment2/MainViewModel2.kt
+++ app/src/main/java/com/example/bd0631/baseproject/mainFragment2/MainViewModel2.kt
@@ -1,14 +1,12 @@
 package com.example.bd0631.baseproject.mainFragment2
 
 import android.arch.lifecycle.MutableLiveData
+import com.example.bd0631.baseproject.base.BaseViewModel
 import com.example.bd0631.baseproject.data.local.BaseRepository
 import com.example.bd0631.baseproject.data.remote.Post
 import com.example.bd0631.baseproject.data.remote.PostApi
-import com.example.bd0631.baseproject.base.BaseViewModel
-import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
-import io.reactivex.schedulers.Schedulers
+import io.reactivex.rxkotlin.subscribeBy
 import javax.inject.Inject
 
 class MainViewModel2 @Inject constructor(
@@ -20,24 +18,16 @@ class MainViewModel2 @Inject constructor(
   val post = MutableLiveData<Post>()
 
   fun onTextClick() {
-    postApi.getPosts()
-        .subscribeOn(Schedulers.newThread())
-        .observeOn(AndroidSchedulers.mainThread())
-        .subscribe(object : Observer<Post> {
-          override fun onComplete() {
-          }
-
-          override fun onSubscribe(d: Disposable) {
-          }
-
-          override fun onNext(t: Post) {
-            post.value = t
-          }
-
-          override fun onError(e: Throwable) {
-          }
-        })
-
     isLoading.value = true
+    postApi.getPosts()
+        .observeOn(AndroidSchedulers.mainThread())
+        .doFinally { isLoading.value = false }
+        .subscribeBy(
+            onNext = {
+              post.value = it
+            }
+
+        )
+
   }
 }
